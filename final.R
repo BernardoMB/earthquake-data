d44388403a0d77f9544df7fd6f55f2da3d438fa6
Bernardo <bmondragonbrozon@gmail.com>
2018-05-19 23:26:40 -0500

    Avance

--- final.R
+++ final.R
@@ -1,63 +1,77 @@
-rm(list=ls())
-
-#Instalamos los paquetes a utilizar
+# Instalamos los paquetes a utilizar
 install.packages("evd")
-install.packages("evir")
-install.packages("fExtremes")
-install.packages("ggplot2")
-install.packages("POT")
-install.packages("PtProcess")
-install.packages("PerformanceAnalytics")
 library(evd)
+install.packages("PerformanceAnalytics")
 library(PerformanceAnalytics)
+
+# Extreme values in R
+# ftp://cran.r-project.org/pub/R/web/packages/evir/evir.pdf
+install.packages("evir")
 library(evir)
+
+install.packages("fExtremes")
 library(fExtremes)
+install.packages("ggplot2")
 library(ggplot2)
+install.packages("POT")
 library(POT)
+install.packages("PtProcess")
 library(PtProcess)
 install.packages('copula')
 library('copula')
 install.packages('Kendall')
 library('Kendall')
 
-#Leemos nuestros datos desde excel en formato csv
-Datos <-read.csv("terremotos.csv")
+# Read data
+data <-read.csv("terremotos.csv")
 
-Datos$Index<-c(1:9220)
-names(Datos)<-c("Tiempo","Magnitud")
+# Change data frame names
+names(data) <- c("time","magnitudee")
 
-#Grafica de análidis exploratorio de datos
-barplot(Datos$Magnitud,xlab="Terremotos en Orden Cronológico\n1950-2015",ylab="Magnitud")
+# Grafica de an?lidis exploratorio de data (Serie de tiempo)
+barplot(data$magnitude,xlab="Terremotos en Orden Cronologico\n1950-2015",ylab="Magnitud")
+
+
 #Histograma en log
-hist(log(Datos$Magnitud),breaks=30,xlab="Magnitud del terremoto\nEn escala logarítmica",ylab="Frecuencia",main="Histograma")
-plot(density(log(Datos$Magnitud)),main="Densidad de la log")
-#QQ plot exponencial vs Datos
-qqplot(Datos$Magnitud,rexp(2000,1),main="QQ plot",ylab="Exponencial",xlab="Magnitud del terremoto")
+hist(log(data$magnitude),breaks=30,xlab="magnitude del terremoto\nEn escala logar?tmica",ylab="Frecuencia",main="Histograma")
+plot(density(log(data$magnitude)),main="Densidad de la log")
+
+
+#QQ plot exponencial vs data
+qqplot(data$magnitude ,rexp(2000,1), main="QQ plot",ylab="Exponencial",xlab="magnitude del terremoto")
 abline(0,.15)
-#Distribución empirica
-ECDF<- ecdf(Datos$Magnitud)
-plot(ECDF,main="Distribucion Empirica de los Datos")
+
+
+#Distribuci?n empirica
+ECDF<- ecdf(data$magnitude)
+plot(ECDF,main="Distribucion Empirica de los data")
+
+
+
 #Medias en exceso
-meplot(Datos$Magnitud,main="Mean excess plot")
+meplot(data$magnitude,main="Mean excess plot")
+
+
+
+
 
 #Distribuciones con las que vamos a comparar
 LOG<-rlnorm(45, meanlog = 0, sdlog = 1)
 P<- ecdf(LOG)
 plot(P,col="blue",main="Distribucion de la lognormal")
+
 PARETO<- rpareto(45, 1, 1)
 PA<- ecdf(PARETO)
 plot(PA,col="red",main="Distribucion de la Pareto")
+
 GPD<- rgpd(45, .497, 10,6.98)
 G<- ecdf(GPD)
 plot(G,col="green",main="Distribucion de la GDP")
+
 par(mfrow=c(2,2))
 plot(P,col="blue",main="Distribucion de la lognormal")
 plot(PA,col="red",main="Distribucion de la Pareto")
 plot(G,col="green",main="Distribucion de la GDP")
 
-install.packages('copula')
-library('copula')
-install.packages('Kendall')
-library('Kendall')
 
 
